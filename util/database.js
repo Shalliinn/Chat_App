2bb5e49c386f0aa6d4b44794ccfdf54dd99bc2dd
Shalliinn <panchalshalin4300@gmail.com>
2022-12-02 10:08:39 +0530

    database change

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
 const Sequelize=require('sequelize')
 
-const sequelize= new Sequelize('ChatApp','root','Spi@4300',{
+const sequelize= new Sequelize(process.env.DB_NAME,process.env.DB_USER_NAME,process.env.DB_PASSWORD,{
     dialect :'mysql',
-    host:'localhost'
+    host:process.env.DB_HOST
 })
 
  module.exports=sequelize;
